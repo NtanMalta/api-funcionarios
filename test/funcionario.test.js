3f98021de56269f3801d01f2271f4a20b8f2f90c
natan <natamalta1941@gmail.com>
2026-07-13 17:11:48 -0300

    feat:adicionando campo salario ao cadastro funcionario

--- test/funcionario.test.js
+++ test/funcionario.test.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const request = require ("supertest")
-
-const app = require("../app")
-
-//teste
-
-describe("API de funcionarios",()=>[
-
-    it("Deve cadastrar um funcionario",async()=>{
-=======
 const request = require("supertest");
 const app = require("../src/app");
 const sequelize = require("../src/config/database");
@@ -17,39 +6,24 @@ const sequelize = require("../src/config/database");
 
 afterAll(async()=>{
 
-    await sequelize.close();
+await sequelize.close();
 
 });
 
 describe("API Funcionarios",()=>{
 
-    it("Deve cadastrar funcionario", async()=>{
->>>>>>> upstream/master
+it("Deve cadastrar funcionario", async()=>{
 
-        const resposta = await request(app)
-        .post("/funcionarios")
-        .send({
-<<<<<<< HEAD
-            nome:"maria",
-            cargo:"Analista"
-        })
+const resposta = await request(app)
+.post("/funcionarios")
+.send({
+nome:"Maria",
+cargo:"Analista"
+});
 
-        expect(resposta.statusCode).toBe(201)
-
-        expect(resposta.body.nome).toBe("maria")
-
-        expect(resposta.body.cargo).toBe("Analista")
-
-    })
-])
-=======
-            nome:"Maria",
-            cargo:"Analista"
-        });
-
-        expect(resposta.statusCode).toBe(201);
-
-    });
+expect(resposta.statusCode).toBe(201);
 
 });
->>>>>>> upstream/master
+    
+
+});
